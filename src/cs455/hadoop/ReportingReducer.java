9b6c3da87c13517aecc79c62e4ca32cd5a5330d9
el-loza <ethan.loza@gmail.com>
2017-04-14 15:46:13 -0600

    Attemped Finish of phase 2 try Three Testing1

--- src/cs455/hadoop/ReportingReducer.java
+++ src/cs455/hadoop/ReportingReducer.java
@@ -21,60 +21,72 @@ public class ReportingReducer extends Reducer <Text, StateDataWritable, Text, Re
     float aveRooms95Perc = 0;
     String mostElderlyState = "NULL";
 
-    public void reduce (Text key, Iterable<StateDataWritable> values, Context context)
-            throws IOException, InterruptedException{
-
+    public void reduce (Text key, Iterable<StateDataWritable> values, Context context) throws IOException, InterruptedException{
         float maxValue = Float.MIN_VALUE;
         List<Float> aveRooms = new ArrayList<Float>();
 
-        for (StateDataWritable value : values) {
-            System.out.println("THE VALUEIS:==================:");
-            System.out.println(value.toString());
-            String kk = value.state;
-            System.out.println(kk);
-            System.out.println(value.toString());
-            System.out.println("Before");
-            StringBuilder sb2 = new StringBuilder();
-            for (Map.Entry<String,StateDataWritable> entry : statesData.entrySet()){
-                sb2.append("The Key is: " + entry.getKey() + "\n");
-                sb2.append("The State is: " + entry.getValue().state + "\n");
-            }
-            System.out.println(sb2.toString());
-
-            statesData.put(kk, value);
-            StringBuilder sb1 = new StringBuilder();
-            for (Map.Entry<String,StateDataWritable> entry : statesData.entrySet()){
-                sb1.append("The Key is: " + entry.getKey() + "\n");
-                sb1.append("The State is: " + entry.getValue().state + "\n");
-            }
-            System.out.println(sb1.toString());
-            if (value.percentElderly > maxValue){
-                maxValue = value.percentElderly;
-                mostElderlyState = kk;
-            }
-            aveRooms.add(value.aveRooms);
-            System.out.println(statesData.get(kk).toString());
+        for(StateDataWritable stateValue : values){
+            System.out.println(stateValue.toString());
         }
-
-        System.out.println("WE ARE GETTIN To the bootm===========");
-        StringBuilder sb = new StringBuilder();
-        for (Map.Entry<String,StateDataWritable> entry : statesData.entrySet()){
-            sb.append("The Key is: " + entry.getKey() + "\n");
-            sb.append("The State is: " + entry.getValue().state + "\n");
-        }
-        System.out.println(sb.toString());
-
-        Collections.sort(aveRooms);
-        int roomIndex = (int) Math.ceil((double) aveRooms.size() * (0.95));
-        System.out.println("ROOM INDEX: " +roomIndex);
-        System.out.println("AVE ROOM SIZE: " + aveRooms.size());
-        aveRooms95Perc = aveRooms.get(roomIndex - 1);
 
         ReportingWritable answ = new ReportingWritable (statesData, aveRooms95Perc, mostElderlyState);
         context.write(new Text("USA"), answ);
-
-
     }
+
+//    public void reduce (Text key, Iterable<StateDataWritable> values, Context context)
+//            throws IOException, InterruptedException{
+//
+//        float maxValue = Float.MIN_VALUE;
+//        List<Float> aveRooms = new ArrayList<Float>();
+//
+//        for (StateDataWritable value : values) {
+//            System.out.println("THE VALUEIS:==================:");
+//            System.out.println(value.toString());
+//            String kk = value.state;
+//            System.out.println(kk);
+//            System.out.println(value.toString());
+//            System.out.println("Before");
+//            StringBuilder sb2 = new StringBuilder();
+//            for (Map.Entry<String,StateDataWritable> entry : statesData.entrySet()){
+//                sb2.append("The Key is: " + entry.getKey() + "\n");
+//                sb2.append("The State is: " + entry.getValue().state + "\n");
+//            }
+//            System.out.println(sb2.toString());
+//
+//            statesData.put(kk, value);
+//            StringBuilder sb1 = new StringBuilder();
+//            for (Map.Entry<String,StateDataWritable> entry : statesData.entrySet()){
+//                sb1.append("The Key is: " + entry.getKey() + "\n");
+//                sb1.append("The State is: " + entry.getValue().state + "\n");
+//            }
+//            System.out.println(sb1.toString());
+//            if (value.percentElderly > maxValue){
+//                maxValue = value.percentElderly;
+//                mostElderlyState = kk;
+//            }
+//            aveRooms.add(value.aveRooms);
+//            System.out.println(statesData.get(kk).toString());
+//        }
+//
+//        System.out.println("WE ARE GETTIN To the bootm===========");
+//        StringBuilder sb = new StringBuilder();
+//        for (Map.Entry<String,StateDataWritable> entry : statesData.entrySet()){
+//            sb.append("The Key is: " + entry.getKey() + "\n");
+//            sb.append("The State is: " + entry.getValue().state + "\n");
+//        }
+//        System.out.println(sb.toString());
+//
+//        Collections.sort(aveRooms);
+//        int roomIndex = (int) Math.ceil((double) aveRooms.size() * (0.95));
+//        System.out.println("ROOM INDEX: " +roomIndex);
+//        System.out.println("AVE ROOM SIZE: " + aveRooms.size());
+//        aveRooms95Perc = aveRooms.get(roomIndex - 1);
+//
+//        ReportingWritable answ = new ReportingWritable (statesData, aveRooms95Perc, mostElderlyState);
+//        context.write(new Text("USA"), answ);
+//
+//
+//    }
 
 
 
